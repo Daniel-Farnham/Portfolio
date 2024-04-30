94aa407ca6d9e95707f7a5ccf8c724612694ad1c
Daniel <>
2024-04-30 12:02:00 +1000

    Task: fix form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -8,51 +8,58 @@ function ContactForm() {
     message: '',
   });
 
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setFormData((prevState) => ({
-  //     ...prevState,
-  //     [name]: value,
-  //   }));
-  // };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+      body: encode({ "form-name": "contact", ...formData })
     })
       .then(() => alert("Success!"))
       .catch(error => alert(error));
-
-    e.preventDefault();
   };
 
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" value={message} onChange={this.handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    );
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
   }
 
+  return (
+    <form onSubmit={handleSubmit} data-netlify="true" name="contact">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>
+          Your Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:
+          <textarea name="message" value={formData.message} onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
+}
 
 export default ContactForm;
